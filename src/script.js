c097d974343deeebc87b8e44a0cfe09708c6c18d
primetime71 <shantanu3543@gmail.com>
2021-11-15 00:24:41 +0530

    refractor a lot of things. Modified all files

--- src/script.js
+++ src/script.js
@@ -1,90 +1,119 @@
-function randomNumber1(){
-    // let num = Math.floor(Math.random()+6);
-    let num=Math.floor(Math.random()*6+1);
-    console.log("r1:" + num)
-    return num;
+
+
+
+function startScript(){
+let randomNumber1=Math.floor(Math.random()*6+1);
+let randomNumber2=Math.floor(Math.random()*6+1);
+
+const imgSource="../images/";
+
+let randomImg1=imgSource+"dice"+randomNumber1+".png";
+document.getElementById("dice-1").setAttribute("src",randomImg1);
+
+let randomImg2=imgSource+"dice"+randomNumber2+".png";
+document.getElementById("dice-2").setAttribute("src",randomImg2);
+
+
+if(randomNumber1>randomNumber2){
+    document.getElementById("title").innerHTML="Player 1 Wins! 😀";
+}
+else if(randomNumber2>randomNumber1){
+    document.getElementById("title").innerHTML="Player 2 Wins! 👍";
+}
+else{
+    document.getElementById("title").innerHTML="It's a Draw! 🤡";
+}
 }
 
-function randomNumber2(){
-    // let num = Math.floor(Math.random()+6);
-    let num=Math.floor(Math.random()*6+1);
-    console.log("r2:" + num)
-    return num;
-}
-let r1=randomNumber1();
-let r2=randomNumber2();
 
-function result(){ // writes the result to header section
+// function randomNumber1(){
+//     // let num = Math.floor(Math.random()+6);
+//     let num=Math.floor(Math.random()*6+1);
+//     console.log("r1:" + num)
+//     return num;
+// }
 
-let ans;
+// function randomNumber2(){
+//     // let num = Math.floor(Math.random()+6);
+//     let num=Math.floor(Math.random()*6+1);
+//     console.log("r2:" + num)
+//     return num;
+// }
 
-if(r1>r2){
-    // alert("r1 is greater");
-    ans="Player 1 Wins!";
-    document.getElementById("title").innerText=ans;
-}
-else if(r2>r1){
-    // alert("r2 is greater")
-    ans="Player 2 Wins!";
-    document.getElementById("title").innerText=ans  ;
-}
-else {
-    // alert("draw");
-    ans="It's a Draw";
-    document.getElementById("title").innerText=ans;
-}
+// let r1=randomNumber1();
+// let r2=randomNumber2();
 
-}
 
-function setResult(){
-    // let imgNumber1=randomNumber1();
-    // let imgNumber2=randomNumber2();
+// function result(){ // writes the result to header section
 
-        switch(r1){
-            case 1:
-                document.getElementById("dice-1").setAttribute("src","../images/dice1.png");
-                  break;
-            case 2:
-                document.getElementById("dice-1").setAttribute("src","../images/dice2.png");
-                    break;   
-            case 3:
-                document.getElementById("dice-1").setAttribute("src","../images/dice3.png");
-                    break;   
-            case 4:
-                document.getElementById("dice-1").setAttribute("src","../images/dice4.png");
-                    break;   
-            case 5:
-                document.getElementById("dice-1").setAttribute("src","../images/dice5.png");
-                    break;   
-            case 6:
-                document.getElementById("dice-1").setAttribute("src","../images/dice6.png");
-                    break;    
+// let ans;
+// if(r1>r2){
+//     // alert("r1 is greater");
+//     ans="Player 1 Wins!";
+//     document.getElementById("title").innerText=ans;
+// }
+// else if(r2>r1){
+//     // alert("r2 is greater")
+//     ans="Player 2 Wins!";
+//     document.getElementById("title").innerText=ans  ;
+// }
+// else {
+//     // alert("draw");
+//     ans="It's a Draw";
+//     document.getElementById("title").innerText=ans;
+// }
+
+// }
+
+// function setResult(){
+//     // let imgNumber1=randomNumber1();
+//     // let imgNumber2=randomNumber2();
+        
+//         switch(r1){
+//             case 1:
+//                 document.getElementById("dice-1").setAttribute("src","../images/dice1.png");
+//                   break;
+//             case 2:
+//                 document.getElementById("dice-1").setAttribute("src","../images/dice2.png");
+//                     break;   
+//             case 3:
+//                 document.getElementById("dice-1").setAttribute("src","../images/dice3.png");
+//                     break;   
+//             case 4:
+//                 document.getElementById("dice-1").setAttribute("src","../images/dice4.png");
+//                     break;   
+//             case 5:
+//                 document.getElementById("dice-1").setAttribute("src","../images/dice5.png");
+//                     break;   
+//             case 6:
+//                 document.getElementById("dice-1").setAttribute("src","../images/dice6.png");
+//                     break;    
                 
 
-        }
+//         }
 
 
-        switch(r2){
-            case 1:
-                document.getElementById("dice-2").setAttribute("src","../images/dice1.png");
-                  break;
-            case 2:
-                document.getElementById("dice-2").setAttribute("src","../images/dice2.png");
-                    break;   
-            case 3:
-                document.getElementById("dice-2").setAttribute("src","../images/dice3.png");
-                    break;   
-            case 4:
-                document.getElementById("dice-2").setAttribute("src","../images/dice4.png");
-                    break;   
-            case 5:
-                document.getElementById("dice-2").setAttribute("src","../images/dice5.png");
-                    break;   
-            case 6:
-                document.getElementById("dice-2").setAttribute("src","../images/dice6.png");
-                    break;    
+//         switch(r2){
+//             case 1:
+//                 document.getElementById("dice-2").setAttribute("src","../images/dice1.png");
+//                   break;
+//             case 2:
+//                 document.getElementById("dice-2").setAttribute("src","../images/dice2.png");
+//                     break;   
+//             case 3:
+//                 document.getElementById("dice-2").setAttribute("src","../images/dice3.png");
+//                     break;   
+//             case 4:
+//                 document.getElementById("dice-2").setAttribute("src","../images/dice4.png");
+//                     break;   
+//             case 5:
+//                 document.getElementById("dice-2").setAttribute("src","../images/dice5.png");
+//                     break;   
+//             case 6:
+//                 document.getElementById("dice-2").setAttribute("src","../images/dice6.png");
+//                     break;    
                 
 
-        }
+//         }
 
-}
+// }
